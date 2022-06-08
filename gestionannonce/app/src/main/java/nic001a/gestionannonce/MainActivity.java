da7f8965496320607601af7442229e6c2e2af1aa
nic001a <nicola.info.dimarco@gmail.com>
2022-06-08 22:34:54 +0100

    finished the first screen

--- gestionannonce/app/src/main/java/nic001a/gestionannonce/MainActivity.java
+++ gestionannonce/app/src/main/java/nic001a/gestionannonce/MainActivity.java
@@ -1,30 +1,15 @@
 package nic001a.gestionannonce;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
-    ListView l;
-    String tutorials[]
-            = { "Algorithms", "Languages",
-            "Data Structures", "Interview Corner",
-            "GATE", "ISRO CS",
-            "UGC NET CS", "CS Subjects",
-            "Web Technologies" };
-    @Override
+public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.guest_activity);
-        l = findViewById(R.id.list);
-        ArrayAdapter<String> arr;
-        arr
-                = new ArrayAdapter<String>(
-                this,
-                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
-                tutorials);
-        l.setAdapter(arr);
+        setContentView(R.layout.main_activity);
     }
+
+
 }
