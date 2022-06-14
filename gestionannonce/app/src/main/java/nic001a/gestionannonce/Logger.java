aa6315f17d28024509b0829ff5257d565dfbeb0b
nic001a <nicola.info.dimarco@gmail.com>
2022-06-14 16:00:43 +0100

    finished login (without login) todo : edit page

--- gestionannonce/app/src/main/java/nic001a/gestionannonce/Logger.java
+++ gestionannonce/app/src/main/java/nic001a/gestionannonce/Logger.java
@@ -1,14 +1,41 @@
 package nic001a.gestionannonce;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Logger extends AppCompatActivity {
+
+    Button login_button;
+    EditText mail;
+    EditText password;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        login_button = findViewById(R.id.Login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login_();
+            }
+        });
+
+    }
+
+
+    protected void login_() {
+       // if (mail.getText().equals("admin@seller.com") && password.getText().equals(("admin"))) {
+            Intent intent = new Intent(this, Editor.class);
+            startActivity(intent);
+      //  }
     }
 }
--- gestionannonce/app/src/main/java/nic001a/gestionannonce/MainActivity.java
+++ gestionannonce/app/src/main/java/nic001a/gestionannonce/MainActivity.java
@@ -35,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //END OF ON_CREATE
+
     private void goToSellerActivity() {
         Intent switchActivity = new Intent(this, Logger.class);
         startActivity(switchActivity);
