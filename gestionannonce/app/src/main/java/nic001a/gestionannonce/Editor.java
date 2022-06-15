edc0e8bf75aabfc25c0017825d76b51ca3e0a01e
nic001a <nicola.info.dimarco@gmail.com>
2022-06-15 12:28:05 +0100

    fixed Editor page view (todo : login)

--- gestionannonce/app/src/main/java/nic001a/gestionannonce/Editor.java
+++ gestionannonce/app/src/main/java/nic001a/gestionannonce/Editor.java
@@ -1,16 +1,15 @@
 package nic001a.gestionannonce;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 
 public class Editor extends AppCompatActivity {
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.edit_);
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_activity);
     }
 }
